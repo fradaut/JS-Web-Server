06717667578ce650fc2c840852e4bc12f00fc0d5
NFalone <qoo19830526@gmail.com>
2023-04-23 19:39:51 +0800

    feat: 增加SSL連線方式 1. SSL檔案設定 2. HTTPS連線設定

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import serve from 'koa-static';
+import fs from 'fs';
+import https from 'https';
+// import sslify, { xForwardedProtoResolver } from 'koa-sslify';
 
 const app = new Koa();
 const router = new Router();
+
+// app.use(sslify.default({ resolver: xForwardedProtoResolver, customProtoHeader: 'X-Forwarded-Proto' }));
 
 const staticPath = './vue3-jsweb/dist';
 app.use(serve(staticPath));
@@ -15,7 +20,15 @@ router.get('/api/hello', async (ctx) => {
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-const port = 3001;
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+app.listen(3000, () => {
+    console.log(`server is running on port 3000`);
+});
+
+const sslOptions = {
+    key: fs.readFileSync('./privkey.pem'),
+    cert: fs.readFileSync('./fullchain.pem'),
+};
+
+https.createServer(sslOptions, app.callback()).listen(3001, () => {
+    console.log('Koa.js app listening on port 3001 with SSL');
 });
