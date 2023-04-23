a175aff5f141fd9d1eaed40c684efd39459031b3
NFalone <qoo19830526@gmail.com>
2023-04-23 16:22:01 +0800

    feat: 新頁面組件套用 套用新組建 a 進入新頁面

--- vue3-jsweb/src/main.js
+++ vue3-jsweb/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import App from './vuedoc/App.vue'
+import A from './A.vue'
 
 createApp(App).mount('#app')
+createApp(A).mount('#a')
