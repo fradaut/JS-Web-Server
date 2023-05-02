6c9b850ad73f485e850f9cddf848d9fa1057d622
NFalone <qoo19830526@gmail.com>
2023-05-02 18:35:22 +0800

    refactor: 前端分離 前端部分由其他程式處理.

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 import Koa from 'koa';
 import Router from 'koa-router';
-import serve from 'koa-static';
-import fs from 'fs';
-import https from 'https';
-// import sslify, { xForwardedProtoResolver } from 'koa-sslify';
 
 const app = new Koa();
 const router = new Router();
-
-// app.use(sslify.default({ resolver: xForwardedProtoResolver, customProtoHeader: 'X-Forwarded-Proto' }));
-
-const staticPath = './vue3-jsweb/dist';
-app.use(serve(staticPath));
 
 router.get('/api/hello', async (ctx) => {
     ctx.body = 'hello';
@@ -22,13 +13,4 @@ app.use(router.allowedMethods());
 
 app.listen(3000, () => {
     console.log(`server is running on port 3000`);
-});
-
-const sslOptions = {
-    key: fs.readFileSync('./privkey.pem'),
-    cert: fs.readFileSync('./fullchain.pem'),
-};
-
-https.createServer(sslOptions, app.callback()).listen(3001, () => {
-    console.log('Koa.js app listening on port 3001 with SSL');
 });
