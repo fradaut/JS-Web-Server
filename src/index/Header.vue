9a976ddc21368aa1283cd62e5a83dc5f2b8677f1
NFalone <qoo19830526@gmail.com>
2023-05-07 03:49:48 +0800

    feat: 首頁Header設計. header組件. nav組件.

---
+++ src/index/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <header class="header">
+        <div class="title" @click="handleTitleClick">Title</div>
+        <nav class="nav">
+            <NavLink label="Vue3" :to="linkA" />
+            <NavLink label="MC" :to="linkB" />
+            <NavLink label="IoT" :to="linkC" />
+        </nav>
+        <div class="menu" @click="handleMenuClick">Menu</div>
+    </header>
+</template>
+  
+<script setup>
+    import NavLink from '../components/index/Nav.vue';
+  
+    const linkA = './vuedoc';
+    const linkB = './minecraft';
+    const linkC = './IoT';
+  
+    function handleTitleClick() {
+        // Title click logic here
+        console.log('Title clicked');
+    }
+  
+    function handleMenuClick() {
+        // Menu click logic here
+        console.log('Menu clicked');
+    }
+</script>
+  
+<style scoped>
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        height: 50px;
+        background-color: #ffd9e6;
+        padding: 0 20px;
+        box-sizing: border-box;
+    }
+  
+    .title,
+    .menu {
+        cursor: pointer;
+    }
+</style>
---
+++ src/index/index.vue
@@ -0,0 +1,18 @@
+<!-- src/pages/NewPage.vue -->
+<script setup>
+    import Header from './Header.vue'
+</script>
+
+<template>
+  <Header />
+  <div class="content">
+    <h2>首頁</h2>
+    <!-- 在此處添加頁面內容 -->
+  </div>
+</template>
+
+<style scoped>
+.content {
+  margin-top: 5rem;
+}
+</style>
