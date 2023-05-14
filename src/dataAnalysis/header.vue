d0bece854f1fc4c1987c68773b50583e8c9cac4c
NFalone <qoo19830526@gmail.com>
2023-05-14 17:35:59 +0800

    refactor: 刪除多餘程式碼. 移除非必要的程式碼.

--- src/dataAnalysis/header.vue
+++ src/dataAnalysis/header.vue
@@ -1,35 +1,11 @@
 <template>
     <header>
-        <div class="title" @click="handleTitleClick">Title</div>
         <upload />
-        <nav class="nav">
-            <NavLink label="Vue3" :to="vue" />
-            <NavLink label="MC" :to="mc" />
-            <NavLink label="IoT" :to="iot" />
-            <NavLink label="D.A." :to="da" />
-        </nav>
-        <div class="menu" @click="handleMenuClick">Menu</div>
     </header>
 </template>
 
 <script setup>
-    import NavLink from '../components/index/Nav.vue';
     import upload from './upload.vue';
-  
-    const vue = '#';
-    const mc = '#';
-    const iot = '#';
-    const da = '#';
-  
-    function handleTitleClick() {
-        // Title click logic here
-        console.log('Title clicked');
-    }
-  
-    function handleMenuClick() {
-        // Menu click logic here
-        console.log('Menu clicked');
-    }
 </script>
   
 <style scoped lang="scss">
@@ -45,10 +21,5 @@
         background-color: #ffd9e6;
         padding: 0 20px;
         box-sizing: border-box;
-    }
-  
-    .title,
-    .menu {
-        cursor: pointer;
     }
 </style>
